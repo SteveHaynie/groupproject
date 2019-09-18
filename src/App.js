303f679e5201226f80dbd990fbef708659ecf1be
STEPHEN HAYNIE <dusk996@Steves-MacBook-Pro-2.local>
2019-09-17 19:53:02 -0600

    update folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Login from "./components/login/login.js";
+// import ManagementLanding from "./components/management/managementlanding/managementlanding.js";
 
-function App() {
+class App extends React.Component {
+
+
+
+
+  render () {
   return (
     <div className="App">
-     
+  
+
+     <Router>
+      <Switch>
+{/* 
+      <Route
+              exact
+              path="/"
+              render={props => {
+                return (
+                  <div className="App">
+                    <Login {...props} />
+                    
+                  </div>
+                );
+              }}
+            />
+      <Route
+              exact
+              path="/"
+              render={props => {
+                return (
+                  <div className="App">
+                    <ManagementLanding {...props} />
+                    
+                  </div>
+                );
+              }}
+            /> */}
+
+
+      </Switch>
+     </Router>
     </div>
   );
+}
 }
 
 export default App;
