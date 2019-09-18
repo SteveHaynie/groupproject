37f5cf216ab6c0d18c0169ef50ed35a815bfa31b
adporter96 <adporter96@gmail.com>
2019-09-17 20:05:10 -0600

    managerbranch

---
+++ src/components/management/landingpagemanager/managementlandingpage.js
@@ -0,0 +1,15 @@
+import React from react
+import './managementlandingpage.css'
+
+class managementlandingpage extends React.Component{
+
+    render(){
+        return (
+            <div className='managementHomePage'>
+                Management Landing Page
+            </div>
+        )
+    }
+}
+
+export default managementlandingpage;
