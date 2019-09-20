f83610fb7d3d530a4ac095d292e9e2af1440ea83
Zachary Saenz <ztsaenz@gmail.com>
2019-09-19 19:09:38 -0600

    working server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const app = require("express")();
 const session = require("express-session");
 const bodyParser = require("body-parser");
@@ -7,20 +8,24 @@ const path = require("path");
 
 app.use(cors());
 
-massive().then(db=> {
+massive(`postgres://etxtbikbqxorhi:b344733ddc2c2e214eed2919f775950d7b39d84c420113937fe9771674134847@ec2-107-21-201-238.compute-1.amazonaws.com:5432/de29a6362fjkbr?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
+
+`).then(db=> {
     console.log('connect to db')
     app.set("db", db)
+    
 })
 .catch(error => console.error(error));
 
 app.use(session({
+    secret: 'keyboad cat',
     maxAge: 50000000000,
     resave: true,
     saveUninitialized: true
 }));
 
 app.use(express.static(path.join(__dirname, "build")))
-app.use(bodyParse.json())
+app.use(bodyParser.json())
 
 app.listen(process.env.PORT || 8080, ()=> {
     console.log('listening')
