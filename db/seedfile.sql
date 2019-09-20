cfd4667ad333787b88e3400a754451a074e51282
Zachary Saenz <ztsaenz@gmail.com>
2019-09-19 20:20:32 -0600

    conflicts

--- db/seedfile.sql
+++ db/seedfile.sql
@@ -1,33 +1,38 @@
 drop table if EXISTS work_order_archive, work_orders, unit_table, messages, messages_archive, users;
 
-CREATE TABLE messages (
-id SERIAL primary key,
-user_id INTEGER REFERENCES users (id),
-msg_discription VARCHAR(255),
-msg_created_at TIMESTAMP,
-msg_notes VARCHAR(255)
+CREATE TABLE users
+(
+id serial primary key,
+first_name text,
+last_name text,
+password text,
+email text,
+administrator boolean,
+unit_id INTEGER
 );
 
-CREATE TABLE messages_archive (
+CREATE TABLE messages
+(
+id SERIAL primary key,
+user_id INTEGER REFERENCES users (id),
+msg_discription text,
+msg_created_at TIMESTAMP,
+msg_notes text
+);
+
+CREATE TABLE messages_archive
+(
 id Serial primary key,
 user_id INTEGER REFERENCES users (id),
-discription VARCHAR(255),
-notes VARCHAR(255),
-created_at VARCHAR(255),
+discription text,
+notes text,
+created_at text,
 completed_at TIMESTAMP
 );
 
-CREATE TABLE users (
-id serial primary key,
-first_name VARCHAR(255),
-last_name VARCHAR(255),
-password VARCHAR(255),
-email VARCHAR(255),
-administrator boolean,
-unit_id INTEGER(255)
-);
 
-CREATE TABLE unit_table (
+CREATE TABLE unit_table
+(
 id serial primary key,
 unit_number VARCHAR,
 unit_type VARCHAR,
@@ -35,22 +40,23 @@ unit_bedrooms INTEGER,
 unit_baths INTEGER,
 unit_sq_footage INTEGER,
 unit_rent FLOAT,
-amenities VARCHAR(255)
+amenities text
 );
 
 CREATE TABLE work_orders (
 id serial primary key,
 unit_id INTEGER,
-discription VARCHAR(255),
+discription text,
 created_at TIMESTAMP,
 photo text
 );
 
-CREATE TABLE work_order_archive (
+CREATE TABLE work_order_archive
+(
 id serial primary key,
 unit_id INTEGER,
-discription VARCHAR(255),
-notes VARCHAR(255),
+discription text,
+notes text,
 created_at TIMESTAMP,
 completed_at TIMESTAMP,
 photo text
--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require('express')
+const express = require("express");
 const app = require("express")();
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const massive = require("massive");
 const path = require("path");
+require("dotenv").config()
 
 app.use(cors());
 
-massive(`postgres://etxtbikbqxorhi:b344733ddc2c2e214eed2919f775950d7b39d84c420113937fe9771674134847@ec2-107-21-201-238.compute-1.amazonaws.com:5432/de29a6362fjkbr?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
+massive(process.env.DATABASE_URL).then(db => {
+    console.log("connected to db");
+    app.set("db", db);
+    return db.seedfile();
+  })
+  .catch(error => console.error(error));
 
-`).then(db=> {
-    console.log('connect to db')
-    app.set("db", db)
-    
-})
-.catch(error => console.error(error));
-
-app.use(session({
-    secret: 'keyboad cat',
+app.use(
+  session({
+    secret: "keyboad cat",
     maxAge: 50000000000,
     resave: true,
     saveUninitialized: true
-}));
+  })
+);
 
-app.use(express.static(path.join(__dirname, "build")))
-app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, "build")));
+app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 8080, ()=> {
-    console.log('listening')
-})
+app.listen(process.env.PORT || 8080, () => {
+  console.log("listening");
+});
