7d941a2e463605daf294e80487cce05a09bc9d90
tnjohnson <tjohnson27@gmail.com>
2019-09-21 13:01:45 -0600

    login subscribed to store

--- src/components/management/workorderview/workorderview.js
+++ src/components/management/workorderview/workorderview.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-
-class WorkOrderView extends React.Component{
-    render(){
-        return (
-            <div className='managementHomePage'>
-            <div className='manage-header'>Current Work orders</div>
-            <div className='navbar'>
-                 <Link to='/unitcreation'>Create new unit</Link>
-                <Link to='/users'>View users</Link>
-                <Link to='/workorderview'>View work orders</Link>
-                <Link to ='/login'>Sign out</Link>
-                </div>
-
-            </div>
-        )
-    }
-}
-
-export default WorkOrderView
-=======
 import React from 'react';
 import "./workorderview.css";
 import moment from 'moment';
@@ -86,4 +62,5 @@ render () {
 }
 
 }
->>>>>>> master
+
+export default WorkOrderView;
