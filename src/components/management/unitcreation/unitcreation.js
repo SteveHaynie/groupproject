f303077f409e6dbc13e17804731a54f14a19a487
tnjohnson <tjohnson27@gmail.com>
2019-09-24 21:07:26 -0600

    select option created

--- src/components/management/unitcreation/unitcreation.js
+++ src/components/management/unitcreation/unitcreation.js
@@ -15,15 +15,54 @@ class UnitCreation extends React.Component{
                     <Link to ='/login'>Sign out</Link>
                     </div>
                 <div className='UnitCreationPage'>
-                    <div className='UnitAddress'>Unit Address: </div>
-                    <div className='Amenities'>
-                        <div className='Type'> Type: </div>
-                        <div className='Bedrooms'> Bedrooms: </div>
-                        <div className='Baths'>Baths:</div>
-                        <div className='SqFoot'>Sq Footage:</div>
-                        <div className='AnimalAllowance'> Animals Allowed: </div> 
+                    <div className='UnitAddress'>Unit Address:
+                        <input />
+                     </div>
+                    <div className='Amenities'>Unit Description:
+                        <div className='Type'> Type:
+                            <select>
+                                <option>Basement</option>
+                                <option>Attic</option>
+                                <option>Duplex</option>
+                                <option>Multiplex</option>
+                                <option>Complex</option>
+                                <option>Shed</option>
+                                <option>Nuclear Fall Out Shelter</option>
+
+                            </select>
+                         </div>
+                        <div className='Bedrooms'> Bedrooms: 
+                             <select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+                        <div className='Baths'>Baths:
+                            <select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                            </select>
+                        </div>
+                        <div className='SqFoot'>Sq Footage:
+                            <input />                        
+                        </div>
+                        <div className='AnimalAllowance'> Animals Allowed:
+                            <select>
+                                <option>Please Select an Option</option>                                
+                                <option>True</option>
+                                <option>False</option>
+                            </select>
+                         </div> 
                     </div>
-                    <div className='UnitDescription'></div>
+                    <div className='UnitDescription'>
+                    <input
+                        placeholder="Description"
+                    />
+                    </div>
                     <div className='MonthlyRent'></div>
                     <div className='AdditionalCharges'></div>
                     
