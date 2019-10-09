4d7e35cca66eab9657d63bde47c7e3f85a4a2ae7
tnjohnson <tjohnson27@gmail.com>
2019-10-08 20:50:51 -0600

    we can take fake money soon

--- src/components/tenant/payment/tenantpayment.js
+++ src/components/tenant/payment/tenantpayment.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { Elements, StripeProvider } from "react-stripe-elements";
+import CheckoutForm from "./CheckoutForm";
 
-class TenantPayment extends React.Component {
+
+class TenantPayment extends Component {
+  constructor() {
+    super();
+
+    this.state={
+      partialPayment: "",
+      fullPayment: ""
+    }
+  }
+
+  componentDidMount() {
+    document.title="Pay Rent"
+    // call to the server to get the full rent amount.
+    // axios
+    //   .get("/full_payment")
+    //   .then(response => this.setState({ fullPayment: response.data }))
+    //   .catch(error => console.error(error));
+  }
+  
   render() {
-    return( <div>Payments</div>
+    return(
+      <StripeProvider apiKey="REACT_APP_PUBLISHABLE_KEY">
+        <div className="CreditCardPayment">
+          <h1>Payment:</h1>
+          <div>
+            Rent:{" "}
+            <input
+              value={this.state.partialPayment}
+              onChange={event => this.setState({ partialPayment: event.target.value })}
+            />
+          </div>
+          <Elements>
+            <CheckoutForm
+              partialPayment={this.state.partialPayment}
+            />
+          </Elements>
+        </div>
+      </StripeProvider>
     )
   }
 }
