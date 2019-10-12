0639990d58bb55b02d610dc92a4d4ea03c06d349
STEPHEN HAYNIE <dusk996@Steves-MacBook-Pro-2.local>
2019-10-12 12:26:17 -0600

    updates

--- src/components/tenant/tenantformsubmission/tenantformsubmission.js
+++ src/components/tenant/tenantformsubmission/tenantformsubmission.js
@@ -1,23 +1,76 @@
 import React from "react";
 import "./tenantformsubmission.css";
+import { connect } from "react-redux";
+import axios from "axios";
 
 class TenantFormSubmission extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subject: "",
+      message: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit () {
+    const body ={
+      tenantEmail: this.props.user.email,
+      subject: this.state.subject,
+      message: this.state.message
+    }
+    axios.post("?", body).then(response => {
+      this.props.updateUser(response.data);
+    });
+
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="formsubmissioncontainer">
         <div className="formsubmissionbox">
-          <h1>Form Submit</h1>
-
-
-
-
-
-
-
+          <h1>Comment? Concern? Enter it below:</h1>
+          <textarea
+            className="formtextareasubject"
+            name="subject"
+            type="text"
+            value={this.state.subject}
+            placeholder="Subject"
+            onChange={this.handleChange}
+          />
+          <textarea
+            className="formtextareamessage"
+            name="message"
+            type="text"
+            value={this.state.message}
+            placeholder="Message"
+            onChange={this.handleChange}
+          />
+          <div className ="formnavbar">
+            <button onClick ={this.handleSubmit} className="formsubmitbutton">Send</button>
+            
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default TenantFormSubmission;
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(TenantFormSubmission);
